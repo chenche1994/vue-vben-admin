2951c646e3c4f33f8a8ed09c212df617ed3460a5
chencheng <chenchengxp@126.com>
2023-02-07 14:01:04 +0800

    fix: 加上首页跳转

--- src/views/dashboard/workbench/index.vue
+++ src/views/dashboard/workbench/index.vue
@@ -1,36 +1,21 @@
 <template>
   <PageWrapper>
     <template #headerContent> <WorkbenchHeader /> </template>
-    <div class="lg:flex">
-      <div class="lg:w-7/10 w-full !mr-4 enter-y">
-        <ProjectCard :loading="loading" class="enter-y" />
-        <DynamicInfo :loading="loading" class="!my-4 enter-y" />
-      </div>
-      <div class="lg:w-3/10 w-full enter-y">
-        <QuickNav :loading="loading" class="enter-y" />
-
-        <Card class="!my-4 enter-y" :loading="loading">
-          <img class="xl:h-50 h-30 mx-auto" src="../../../assets/svg/illustration.svg" />
-        </Card>
-
-        <SaleRadar :loading="loading" class="enter-y" />
-      </div>
-    </div>
+    <div>{{ code }}</div>
+    <div>access_token:</div>
+    <div>refresh_token:</div>
   </PageWrapper>
 </template>
 <script lang="ts" setup>
-  import { ref } from 'vue'
-  import { Card } from 'ant-design-vue'
   import { PageWrapper } from '/@/components/Page'
   import WorkbenchHeader from './components/WorkbenchHeader.vue'
-  import ProjectCard from './components/ProjectCard.vue'
-  import QuickNav from './components/QuickNav.vue'
-  import DynamicInfo from './components/DynamicInfo.vue'
-  import SaleRadar from './components/SaleRadar.vue'
+  const href = location.href
+  let startIndex = href.indexOf('?')
+  let endIndex = href.indexOf('#')
+  const code = href.substring(startIndex + 1, endIndex)
 
-  const loading = ref(true)
-
-  setTimeout(() => {
-    loading.value = false
-  }, 1500)
+  window.open(
+    `http://172.19.3.47:8080/oauth/token?grant_type=authorization_code&client_id=tpm&client_secret=abcd1234&redirect_uri=https://172.16.130.135:3100&${code}`,
+    '_blank',
+  )
 </script>
